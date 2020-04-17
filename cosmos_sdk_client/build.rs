0250c9ade6c4fc22bf1d1897d5b023f64ef539f6
Aaron Craelius <aaronc@users.noreply.github.com>
2020-04-17 12:16:43 -0400

    Initial commit

---
+++ cosmos_sdk_client/build.rs
@@ -0,0 +1,22 @@
+use std::io;
+
+fn main() -> Result<(), io::Error> {
+    tonic_build::configure()
+        .compile(
+            &[
+                // "../cosmos-sdk/types/types.proto",
+                // "../cosmos-sdk/x/auth/types/types.proto",
+                // "../cosmos-sdk/x/bank/types/types.proto",
+                // "../cosmos-sdk/x/crisis/types/types.proto",
+                // "../cosmos-sdk/x/distribution/types/types.proto",
+                // "../cosmos-sdk/x/evidence/types/types.proto",
+                // "../cosmos-sdk/x/mint/types/types.proto",
+                // "../cosmos-sdk/x/gov/types/types.proto",
+                // "../cosmos-sdk/x/slashing/types/types.proto",
+                // "../cosmos-sdk/x/staking/types/types.proto",
+                // "../cosmos-sdk/x/upgrade/types/types.proto",
+                "../cosmos-sdk/codec/std/codec.proto",
+            ],
+            &["../cosmos-sdk"],
+        )
+}
---
+++ cosmos_sdk_client/src/lib.rs
@@ -0,0 +1,99 @@
+pub mod tendermint {
+    pub mod libs {
+        pub mod kv {
+            tonic::include_proto!("tendermint.libs.kv");
+        }
+    }
+
+    pub mod crypto {
+        pub mod merkle {
+            tonic::include_proto!("tendermint.crypto.merkle");
+        }
+    }
+
+    pub mod abci {
+        pub mod types {
+            tonic::include_proto!("tendermint.abci.types");
+        }
+    }
+}
+
+pub mod cosmos_sdk {
+    pub mod v1 {
+        tonic::include_proto!("cosmos_sdk.v1");
+    }
+    pub mod x {
+        pub mod auth {
+            pub mod v1 {
+                tonic::include_proto!("cosmos_sdk.x.auth.v1");
+            }
+            pub mod vesting {
+                pub mod v1 {
+                    tonic::include_proto!("cosmos_sdk.x.auth.vesting.v1");
+                }
+            }
+        }
+        pub mod bank {
+            pub mod v1 {
+                tonic::include_proto!("cosmos_sdk.x.bank.v1");
+            }
+        }
+        pub mod crisis {
+            pub mod v1 {
+                tonic::include_proto!("cosmos_sdk.x.crisis.v1");
+            }
+        }
+        pub mod distribution {
+            pub mod v1 {
+                tonic::include_proto!("cosmos_sdk.x.distribution.v1");
+            }
+        }
+        pub mod evidence {
+            pub mod v1 {
+                tonic::include_proto!("cosmos_sdk.x.evidence.v1");
+            }
+        }
+        pub mod gov {
+            pub mod v1 {
+                tonic::include_proto!("cosmos_sdk.x.gov.v1");
+            }
+        }
+        pub mod mint {
+            pub mod v1 {
+                tonic::include_proto!("cosmos_sdk.x.mint.v1");
+            }
+        }
+        pub mod params {
+            pub mod v1 {
+                tonic::include_proto!("cosmos_sdk.x.params.v1");
+            }
+        }
+        pub mod slashing {
+            pub mod v1 {
+                tonic::include_proto!("cosmos_sdk.x.slashing.v1");
+            }
+        }
+        pub mod staking {
+            pub mod v1 {
+                tonic::include_proto!("cosmos_sdk.x.staking.v1");
+            }
+        }
+        pub mod supply {
+            pub mod v1 {
+                tonic::include_proto!("cosmos_sdk.x.supply.v1");
+            }
+        }
+        pub mod upgrade {
+            pub mod v1 {
+                tonic::include_proto!("cosmos_sdk.x.upgrade.v1");
+            }
+        }
+    }
+    pub mod codec {
+        pub mod std {
+            pub mod v1 {
+                tonic::include_proto!("cosmos_sdk.codec.std.v1");
+            }
+        }
+    }
+}
